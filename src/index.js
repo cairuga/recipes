c4bfd5fe0c754c151c2564e89a8f67de326e3afb
Marcelo Cairuga <cairuga@gmail.com>
2021-03-12 13:31:13 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (resquest, response)=> {return response.json({message: "Hello World!"})})
+app.get("/", (resquest, response)=> {return response.json({message: "Hello Marcelo!"})})
 
 app.listen(3333)
